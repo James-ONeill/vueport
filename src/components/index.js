b031aee81867a38a95a5cc2257276130b06e2a1c
James O'Neill <james@jamesoneill.eu>
2018-06-07 19:38:29 +0100

    Export module

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import VueportPageScroll from "./VueportPageScroll.vue";
+import VueportElementPosition from "./VueportElementPosition.vue";
+
+export default {
+    VueportPageScroll,
+    VueportElementPosition
+};
