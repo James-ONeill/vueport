e58652bd4b44617e69686c4ebed6bcd12a01e57a
James O'Neill <james@jamesoneill.eu>
2018-06-10 19:54:20 +0100

    Remove dist folder and add NPM entry point

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import VueportPageScroll from "./VueportPageScroll.vue";
-import VueportElementPosition from "./VueportElementPosition.vue";
-
-export default {
-    VueportPageScroll,
-    VueportElementPosition
-};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import VueportPageScroll from "./components/VueportPageScroll.vue";
+import VueportElementPosition from "./components/VueportElementPosition.vue";
+
+export default {
+  VueportPageScroll,
+  VueportElementPosition
+};
