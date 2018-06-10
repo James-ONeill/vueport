693225ed13afc5d5e2cfecc02f52b5526ada328a
James O'Neill <james@jamesoneill.eu>
2018-06-10 20:09:12 +0100

    Add named exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import VueportPageScroll from "./components/VueportPageScroll.vue";
 import VueportElementPosition from "./components/VueportElementPosition.vue";
 
+export {
+    VueportPageScroll,
+    VueportElementPosition
+};
+
 export default {
   VueportPageScroll,
   VueportElementPosition
